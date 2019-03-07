8a0e43297e320a8f8fdaa3af382bc9e5c3a0ed80
benzakei <benzakei@B016-52-52>
2019-03-07 18:06:21 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        bool Guess(int[] vec, int val)
+        {
+            foreach (var value in vec)
+            {
+                if (value == val) {
+                    return true;
+
+                }
+            }
+            return false;
+        }
+    }
+}
